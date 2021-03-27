a35abae881c824f262aec21dbae9df16e77683aa
Ryan Gieg <47954352+malarkey327@users.noreply.github.com>
2021-03-27 15:32:12 -0700

    Updated Habit and Main files
    Can now store habits in an ArrayList, add new habits, exit correctly, and check off habits

--- src/Habit.java
+++ src/Habit.java
@@ -1,5 +1,55 @@
-public class Habit {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class Habit {
     String name;
-    int track;
+    String type;
+    boolean dailyCheck;
+//----------------------------------------------------------------------------------------------------------------------
+    protected void setName(Scanner in) {
+        System.out.println("Habit Name: ");
+        name = in.nextLine();
+    }
+//----------------------------------------------------------------------------------------------------------------------
+    protected String getName() { return name; }
+//----------------------------------------------------------------------------------------------------------------------
+    protected void setType(Scanner in) {
+        System.out.println("\tSelect a Category\n"
+                + "1: Productivity\n"
+                + "2: Self-Love\n"
+                + "3: Health\n\n"
+                + "Choice: ");
+        try {
+            boolean valid = false;
+            while(!valid) {
+                int choice = in.nextInt();
+                in.nextLine();
+                switch (choice) {
+                    case 1:
+                        type = "Productivity";
+                        valid = true;
+                        break;
+                    case 2:
+                        type = "Self-Love";
+                        valid = true;
+                        break;
+                    case 3:
+                        type = "Health";
+                        valid = true;
+                        break;
+                    default:
+                        System.out.println("** Choice not recognized, try again **");
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("** Input Invalid **");
+            in.nextLine();
+        }
+    }
+//----------------------------------------------------------------------------------------------------------------------
+    protected String getType() { return type; }
+//----------------------------------------------------------------------------------------------------------------------
+    protected boolean getDaily() { return dailyCheck; }
+//----------------------------------------------------------------------------------------------------------------------
+    protected void record() { dailyCheck = true; }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,89 @@
-/*  Marina Hacks project! Plan is to design a mobile app that encourages you to limit grind by growing a healthy
- *  aquarium as you maintain a healthy work balance
+/* Marina Hacks project! Plan is to design a mobile app that encourages you to limit grind by growing a healthy
+aquarium as you maintain a healthy work balance
  */
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+    int enviroState = 0; // Scale 0-3 with 0 being very dirty and 3 being very clean
+    ArrayList<Habit> userHabits = new ArrayList<>(0); // Collection of user created habits, represented by fish
+
     public static void main(String[] args) {
-        new OceanFrame();
+        
+        //new OceanFrame();
         new Main().menu();
     }
+//----------------------------------------------------------------------------------------------------------------------
     public void menu() {
         Scanner in = new Scanner(System.in);
-        System.out.println("Welcome to the beach!");
-        System.out.println("1: Add Habit");
-        System.out.println("2: Record Habit");
-        System.out.println("3: Update Beach");
-        System.out.println("4: Exit");
         try {
-            boolean valid = false;
-            while (!valid) {
-                System.out.print("Choice = ");
+            boolean valid = true;
+            while (valid) {
+                System.out.println("\tWelcome to the beach!\n"
+                + "1: Add Habit\n"
+                + "2: Record Habit\n"
+                + "3: Update Beach\n"
+                + "4: Exit\n\n"
+                + "Choice: ");
                 int choice = in.nextInt();
+                in.nextLine();
                 switch (choice) {
                     case 1:
-                        System.out.println("1");
-                        valid = true;
+                        Habit habit = new Habit();
+                        habit.setName(in);
+                        habit.setType(in);
+                        userHabits.add(habit);
+                        System.out.println(habit.getName() + " Added");
                         break;
                     case 2:
-                        System.out.println("2");
-                        valid = true;
+                        recordSelection(in);
                         break;
                     case 3:
                         System.out.println("3");
-                        valid = true;
                         break;
                     case 4:
-                        System.out.println("4");
-                        System.exit(0);
+                        System.out.println("Exiting, Goodbye!");
+                        valid = false;
+                        break;
                     default:
-                        System.out.println("Choice not recognized, try again");
+                        System.out.println("** Choice not recognized, try again **");
                 }
             }
+            in.close();
+            System.exit(0);
         } catch (InputMismatchException e) {
-            System.out.println("Input Invalid");
+            System.out.println("** Input Invalid **");
             menu();
+        }
+    }
+//----------------------------------------------------------------------------------------------------------------------
+    public void recordSelection(Scanner in) {
+        int i = 0;
+        for (Habit elem : userHabits) {
+            System.out.println(i + ": " + elem.name);
+            i++;
+        }
+        try {
+            boolean valid = false;
+            while(!valid) {
+                System.out.println("Habit Completed: ");
+                int choice = in.nextInt();
+                in.nextLine();
+                if(choice >= userHabits.size() || choice < 0) {
+                    System.out.println("** Out Of Bounds **");
+                } else {
+                    Habit h = userHabits.get(choice);
+                    h.record();
+                    System.out.println(h.getName() + " Completed: " + h.getDaily());
+                    enviroState += 1;
+                    valid = true;
+                }
+            }
+        } catch (IndexOutOfBoundsException | InputMismatchException e) {
+            System.out.println("** Invalid Input **");
+            in.nextLine();
         }
     }
 }
