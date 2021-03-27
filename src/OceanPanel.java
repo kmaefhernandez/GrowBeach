24947f25d496a089c5727d0b1c0e8a462b7d86dc
kmaefhernandez <33080925+kmaefhernandez@users.noreply.github.com>
2021-03-27 14:43:34 -0700

    created a drawing function for bubbles

--- src/OceanPanel.java
+++ src/OceanPanel.java
@@ -8,14 +8,28 @@ public class OceanPanel extends JPanel implements ActionListener {
 
     static final int SCREEN_WIDTH = 870;
     static final int SCREEN_HEIGHT = 600;
-    static final int UNIT_SIZE = 25;
-    static final int OCEAN_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+    //static final int UNIT_SIZE = 25;
+    //static final int OCEAN_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
     Random random;
 
     OceanPanel() {
         this.random = new Random();
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.darkGray);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        drawBubbles(g);
+    }
+
+    public void drawBubbles(Graphics g) {
+        int num_of_bubbles = (random.nextInt(15));
+        g.setColor(Color.white);
+        for(int i = 0; i < num_of_bubbles; i++) {
+            g.fillOval((random.nextInt(870-1) + 100), (random.nextInt(230-1) + 300), 10, 10);
+        }
+
     }
     @Override
     public void actionPerformed(ActionEvent e) {
