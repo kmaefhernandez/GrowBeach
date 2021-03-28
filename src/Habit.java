f7d1de4cdbf218d510b82272a62c03662a439e1d
Ryan Gieg <47954352+Malarkey327@users.noreply.github.com>
2021-03-27 19:34:58 -0700

    Update Habit.java

--- src/Habit.java
+++ src/Habit.java
@@ -1,3 +1,5 @@
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -5,17 +7,24 @@ public class Habit {
     private String name;
     private String type;
     private boolean check;
+    private String frequency;
+    private Instant end;
 
     public Habit() {
         this.name = "Habit";
         this.type = "Type";
         this.check = false;
+        this.frequency = "hourly";
+        this.end = Instant.now().plus(1, ChronoUnit.HOURS);
+
     }
 //----------------------------------------------------------------------------------------------------------------------
-    public Habit(String name, String type, boolean check) {
+    public Habit(String name, String type, boolean check, String frequency, Instant end) {
         this.name = name;
         this.type = type;
         this.check = check;
+        this.frequency = frequency;
+        this.end = end;
     }
 //----------------------------------------------------------------------------------------------------------------------
     protected void setName(Scanner in) {
@@ -61,7 +70,55 @@ public class Habit {
 //----------------------------------------------------------------------------------------------------------------------
     protected String getType() { return type; }
 //----------------------------------------------------------------------------------------------------------------------
+    protected void setFrequency(Scanner in) {
+        System.out.println("\tSelect a Frequency\n"
+                + "1: Daily\n"
+                + "2: Hourly\n\n"
+                + "Choice: ");
+        try {
+            boolean valid = false;
+            while(!valid) {
+                int choice = in.nextInt();
+                in.nextLine();
+                switch (choice) {
+                    case 1:
+                        frequency = "Daily";
+                        end = Instant.now().plus(1, ChronoUnit.DAYS);
+                        valid = true;
+                        break;
+                    case 2:
+                        frequency = "Hourly";
+                        end = Instant.now().plus(1, ChronoUnit.HOURS);
+                        valid = true;
+                        break;
+                    default:
+                        System.out.println("** Choice not recognized, try again **");
+                }
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("** Input Invalid **");
+            in.nextLine();
+        }
+    }
+//----------------------------------------------------------------------------------------------------------------------
+    protected String getFrequency() { return frequency; }
+//----------------------------------------------------------------------------------------------------------------------
     protected boolean getCheck() { return check; }
 //----------------------------------------------------------------------------------------------------------------------
-    protected void record() { check = true; }
+    protected Instant getEnd() { return end; }
+//----------------------------------------------------------------------------------------------------------------------
+    protected void record() {
+        if(Instant.now().isBefore(end)) {
+            System.out.println(this.getName() + " Completed: true");
+            check = true;
+        } else {
+            System.out.println(this.getName() + " Completed: false");
+            check = false;
+        }
+        if(frequency.equals("Hourly")) {
+            end = Instant.now().plus(1, ChronoUnit.HOURS);
+        } else {
+            end = Instant.now().plus(1, ChronoUnit.DAYS);
+        }
+    }
 }
