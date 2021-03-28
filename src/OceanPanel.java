ab9f3a2473ca9f1aaa66b7dcb945441cfd3bdc55
Allison Austin <64452109+allisonaustin@users.noreply.github.com>
2021-03-27 18:30:02 -0700

    Updated Habit and Main files
    Can now store habits in an ArrayList, add new habits, exit correctly, and check off habits

--- src/OceanPanel.java
+++ src/OceanPanel.java
@@ -8,8 +8,8 @@ public class OceanPanel extends JPanel implements ActionListener {
 
     static final int SCREEN_WIDTH = 870;
     static final int SCREEN_HEIGHT = 600;
-    //static final int UNIT_SIZE = 25;
-    //static final int OCEAN_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+    static final int UNIT_SIZE = 25;
+    static final int OCEAN_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
     Random random;
 
     OceanPanel() {
